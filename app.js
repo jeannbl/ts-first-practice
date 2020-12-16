4bcd3a5f354173618ec4dac6b9f198b480cd573b
jeanneth blanco <jeann.merida@gmail.com>
2020-12-16 14:13:58 -0400

    comments deleted.

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var messageJS = "Hello from JavaScript";
 console.log(messageJS);
-var GREETING = "GOOD MORNING"; //(*)
+var GREETING = "GOOD MORNING";
 console.log(GREETING);
-var message = "Hello World, from Type Script"; //(*)
+var message = "Hello World, from Type Script";
 console.log(message);
-//(*) Problems (Cannot redeclared block-scoped variable ´GREETING´) when we compile app.ts to app.js
-//when we open app.js file
 var heading = document.getElementById('greeting');
 heading.textContent = message;
-//Error: document is not defined. This is when is compiled. SO in app.js
